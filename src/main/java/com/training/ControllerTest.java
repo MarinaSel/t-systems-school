d4dd7baf2acb4d7c4d133e4f40d0a8da2d37e9a2
Marina Selivanova <mar.selivanova@mail.ru>
2018-09-25 18:44:18 +0300

    added datasource config

--- src/main/java/com/training/ControllerTest.java
+++ src/main/java/com/training/ControllerTest.java
@@ -1,13 +1,21 @@
 package com.training;
 
-import org.springframework.context.annotation.ComponentScan;
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.sql.DataSource;
 
 @Controller
 public class ControllerTest {
     @RequestMapping(value = "/")
     public String home(){
+        System.out.println(d.toString());
         return "test";
     }
+
+    @Autowired
+    private DataSource d;
+
 }
---
+++ src/main/java/com/training/configurations/DataSourceConfiguration.java
@@ -0,0 +1,28 @@
+package com.training.configurations;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@Component
+@PropertySources({ @PropertySource("classpath:properties/database.properties") })
+public class DataSourceConfiguration {
+
+    @Autowired
+    private Environment env;
+    @Bean
+    public DataSource dataSource() {
+        DataSource dataSource = new DataSource();
+        dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+        dataSource.setUrl(env.getProperty("spring.datasource.url"));
+        dataSource.setUsername(env.getProperty("spring.datasource.username"));
+        dataSource.setPassword(env.getProperty("spring.datasource.password"));
+        return dataSource;
+    }
+}
