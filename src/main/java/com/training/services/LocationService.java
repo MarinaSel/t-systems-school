3fe0ab965275c650f6f1d6c4fa27f77c1f3a543a
Marina Selivanova <mar.selivanova@mail.ru>
2018-11-30 01:25:27 +0300

    added LocationService.findAll and LocationService.findByName methods

--- src/main/java/com/training/services/LocationService.java
+++ src/main/java/com/training/services/LocationService.java
@@ -1,7 +1,25 @@
 package com.training.services;
 
 import com.training.entities.LocationEntity;
+import com.training.models.Location;
+
+import java.util.List;
 
 public interface LocationService extends BaseService<LocationEntity, Long> {
 
+
+    /**
+     * Finds all drivers.
+     *
+     * @return List of all found Driver objects
+     */
+    List<Location> findAll();
+
+    /**
+     * Finds location by name.
+     *
+     * @param name String object with name for search
+     * @return Location object with specified name
+     */
+    Location findByName(String name);
 }
--- src/main/java/com/training/services/impl/LocationServiceImpl.java
+++ src/main/java/com/training/services/impl/LocationServiceImpl.java
@@ -1,17 +1,29 @@
 package com.training.services.impl;
 
 import com.training.entities.LocationEntity;
+import com.training.mappers.LocationMapper;
+import com.training.models.Location;
 import com.training.repositories.LocationRepository;
 import com.training.services.LocationService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 public class LocationServiceImpl implements LocationService {
 
+    private final static Logger LOGGER = LogManager.getLogger(LoadServiceImpl.class);
+
+    private final LocationRepository locationRepository;
+
     @Autowired
-    private LocationRepository locationRepository;
+    public LocationServiceImpl(LocationRepository locationRepository) {
+        this.locationRepository = locationRepository;
+    }
 
     public LocationEntity find(Long id) {
         return locationRepository.getOne(id);
@@ -25,5 +37,20 @@ public class LocationServiceImpl implements LocationService {
     @Transactional
     public void remove(Long id) {
         locationRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Location> findAll() {
+        List<Location> locations = LocationMapper.mapEntityListToModelList(locationRepository.findAll());
+        LOGGER.info("Found all locations");
+        return locations;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Location findByName(String name) {
+        Location location = LocationMapper.mapEntityToModel(locationRepository.findByName(name));
+        LOGGER.info("Found location by name ={}", name);
+        return location;
     }
 }
