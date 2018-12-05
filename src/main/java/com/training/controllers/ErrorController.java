2ba8ca9147ab447d399141bd11b9e5e3dd70d667
Marina Selivanova <mar.selivanova@mail.ru>
2018-12-06 01:59:32 +0300

    added refactoring in ErrorController

--- src/main/java/com/training/controllers/ErrorController.java
+++ src/main/java/com/training/controllers/ErrorController.java
@@ -4,17 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-
 @Controller
 public class ErrorController {
 
     @GetMapping("/error")
-    public ModelAndView errorPage(HttpServletRequest httpRequest) {
-
-        ModelAndView model = new ModelAndView("error");
-        int httpErrorCode = 404;
-        return model;
+    public ModelAndView errorPage() {
+        return new ModelAndView("error");
     }
 
 }
